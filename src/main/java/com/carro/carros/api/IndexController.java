b7f7f7342c1723aa8141309212358ba303cc8d92
William-silva-Developer <william958silva@gmail.com>
2022-09-04 20:18:48 -0300

    segundo commit

--- src/main/java/com/carro/carros/api/IndexController.java
+++ src/main/java/com/carro/carros/api/IndexController.java
@@ -1,9 +1,9 @@
 package com.carro.carros.api;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
+
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +18,20 @@ public class IndexController {
 		return "Olá mundo";
 	};
 	
+	//OBS: ESSE EXEMPLO COM O GETMAPPING FOI APENAS PARA DIFERENCIAR DO POST(O CORRETO É USAR O POST PARA ESCONDER A SENHA MELHOR).
 	@GetMapping("/login")
-	public String LOgin(@RequestParam("login") String login,@RequestParam("senha") String senha) {
+	public String Login(@RequestParam("login") String login,@RequestParam("senha") String senha) {
 		
 		return "login: " +login + " senha: " + senha;
 		
-	}
+	};
+	
+	//OBS: NESTE EXEMPLO FOI USANDO O "PathVariable" NO LUGAR DO "RequestParam"
+	@GetMapping("/login/{login}/senha/{senha}")
+	public String LoginVariable(@PathVariable("login") String login,@PathVariable("senha") String senha) {
+		
+		return "login: " +login + " senha: " + senha;
+		
+	};
+	
 }
